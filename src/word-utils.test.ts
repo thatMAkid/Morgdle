5a5010367e0f2cca2634f7575353ede26fb6daa0
Morgan Anders <MorganAndersDP@gmail.com>
2022-12-16 14:43:40 -0800

    create naive implementation of computeGuess

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,10 +1,53 @@
-import { assert, describe, expect, it } from 'vitest';
-import { getRandomWord } from './word-utils';
-import { render, screen, userEvent } from './test/test-utils';
+import { describe, expect, it } from 'vitest';
+import { computeGuess, getRandomWord, LetterState } from './word-utils';
 
 describe('word-utils', () => {
     it('random word', () => {
         expect(getRandomWord()).toBeTruthy();
         expect(getRandomWord().length).toEqual(5);
     });
+});
+
+
+describe('computeGuess', () => {
+    it('works with match and present', () => {
+        expect(computeGuess('boost', 'basic')).toEqual([
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Present,
+            LetterState.Miss
+        ])
+    });
+
+    it('works with all matches', () => {
+        expect(computeGuess('boost', 'boost')).toEqual([
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match,
+            LetterState.Match
+        ])
+    });
+
+    it('works with full miss', () => {
+        expect(computeGuess('guard', 'boost')).toEqual([
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss
+        ])
+    });
+
+    it('only does one match when two letters are present', () => {
+        expect(computeGuess('solid', 'boost')).toEqual([
+            LetterState.Present,
+            LetterState.Match,
+            LetterState.Miss,
+            LetterState.Miss,
+            LetterState.Miss
+        ])
+    });
+
 });
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -4,3 +4,28 @@ export function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * wordBank.length);
     return wordBank[randomIndex];
 }
+
+export enum LetterState {
+    Miss = 'Miss',
+    Present = 'Present',
+    Match = 'Match',
+}
+
+export function computeGuess(guess: string, answerString: string): LetterState[] {
+    const result: LetterState[] = [];
+
+    const guessArray = guess.split('');
+    const answerArray = answerString.split('');
+
+    guessArray.forEach((letter, index) => {
+        if (letter === answerArray[index]) {
+            result.push(LetterState.Match);
+        } else if (answerArray.includes(letter)) {
+            result.push(LetterState.Present);
+        } else {
+            result.push(LetterState.Miss);
+        }
+    });
+
+    return result;
+}
